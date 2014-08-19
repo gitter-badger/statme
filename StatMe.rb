63a95632ad0a58c044b2750e61b6a8387c889c36
DillonBordeleau <dillontbordeleau@gmail.com>
2014-08-19 11:07:06 -0400

    Location harvest from lat,long tweets

--- StatMe.rb
+++ StatMe.rb
@@ -4,22 +4,22 @@ require 'twitter'
 # Guardian StatMe
 begin
   client = Twitter::Streaming::Client.new do |config|
-    config.consumer_key = "TWPsNBca4joN3o3bF5GP8X52J"
-    config.consumer_secret = "k1O9YO267OsDedaErkhR1MTIxJS9FjgIPCnhtcsM0o4IVEnQk0"
-    config.access_token = "2199777559-0cnOjV5oBtGl57FIWrtpNI5THdrRjyS9f9O9pQV"
-    config.access_token_secret = "hSs76D1kxjepVUuXoy0WinWisUyYdXifEbqNkd7IKCJZ2"
+    config.consumer_key = ""
+    config.consumer_secret = ""
+    config.access_token = ""
+    config.access_token_secret = ""
   end
   puts "Scanning..."
   client.filter(:track => "guardiancrime statme") do |tweet|
     if tweet.geo
-      long = tweet.geo.coordinates[0]
       lat = tweet.geo.coordinates[0]
+      long = tweet.geo.coordinates[1]
       puts "@#{tweet.user.screen_name} #{lat}, #{long}: #{tweet.text}"
       client = Twitter::REST::Client.new do |config|
-        config.consumer_key = "TWPsNBca4joN3o3bF5GP8X52J"
-        config.consumer_secret = "k1O9YO267OsDedaErkhR1MTIxJS9FjgIPCnhtcsM0o4IVEnQk0"
-        config.access_token = "2199777559-0cnOjV5oBtGl57FIWrtpNI5THdrRjyS9f9O9pQV"
-        config.access_token_secret = "hSs76D1kxjepVUuXoy0WinWisUyYdXifEbqNkd7IKCJZ2"
+        config.consumer_key = ""
+        config.consumer_secret = ""
+        config.access_token = ""
+        config.access_token_secret = ""
       end 
       client.update("@#{tweet.user.screen_name} hello!")
       puts "Threat neutralized."
