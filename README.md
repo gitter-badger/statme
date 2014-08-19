4eea1694919396d96c54d3c2f4edc759512e01fa
DillonBordeleau <dillontbordeleau@gmail.com>
2014-08-19 10:22:58 -0400

    Security and name update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-###TwitterStats    
+###Guardian StatMe    
 >Project built to automate twitter responses equipped with crime statistics localized to a user's location.  
--- twitterstats.rb
+++ twitterstats.rb
@@ -1,26 +1,22 @@
 require 'rubygems'
 require 'twitter'
 
-# TwitterStats
+# Guardian StatMe
 begin
-
-# Configuration
   client = Twitter::Streaming::Client.new do |config|
-    config.consumer_key = "TWPsNBca4joN3o3bF5GP8X52J"
-    config.consumer_secret = "k1O9YO267OsDedaErkhR1MTIxJS9FjgIPCnhtcsM0o4IVEnQk0"
-    config.access_token = "2199777559-0cnOjV5oBtGl57FIWrtpNI5THdrRjyS9f9O9pQV"
-    config.access_token_secret = "hSs76D1kxjepVUuXoy0WinWisUyYdXifEbqNkd7IKCJZ2"
+    config.consumer_key = ""
+    config.consumer_secret = ""
+    config.access_token = ""
+    config.access_token_secret = ""
   end
-  
   puts "Scanning..."
-
   client.filter(:track => "guardiancrime statme") do |tweet|
     puts "@#{tweet.user.screen_name}: #{tweet.text}"
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key = "TWPsNBca4joN3o3bF5GP8X52J"
-      config.consumer_secret = "k1O9YO267OsDedaErkhR1MTIxJS9FjgIPCnhtcsM0o4IVEnQk0"
-      config.access_token = "2199777559-0cnOjV5oBtGl57FIWrtpNI5THdrRjyS9f9O9pQV"
-      config.access_token_secret = "hSs76D1kxjepVUuXoy0WinWisUyYdXifEbqNkd7IKCJZ2"
+      config.consumer_key = ""
+      config.consumer_secret = ""
+      config.access_token = ""
+      config.access_token_secret = ""
     end 
     client.update("@#{tweet.user.screen_name} hello!")
     puts "Threat neutralized."
