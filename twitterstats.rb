7ed15111e02699197ee56efa5540f26ca3eec071
DillonBordeleau <dillontbordeleau@gmail.com>
2014-08-18 22:14:47 -0400

    Update for new twitter

--- twitterstats.rb
+++ twitterstats.rb
@@ -6,22 +6,24 @@ begin
 
 # Configuration
   client = Twitter::Streaming::Client.new do |config|
-    config.consumer_key = "kao1cND8RFCHcUJeb5xONcsZQ"
-    config.consumer_secret = "7L6jS0vRnqVVT9WpH7BZ3jPvrYgmd3XinK3tScVEov48wDgzeg"
-    config.access_token = "2199777559-2Eetn8D3VvuLkVKPvWsUA1jNaIgMoDu9F0gBZCT"
-    config.access_token_secret = "7V14U9JLsKEHvdAMpwrqDNuQsVuOnFUGgrO106b6GIOMS"
+    config.consumer_key = "TWPsNBca4joN3o3bF5GP8X52J"
+    config.consumer_secret = "k1O9YO267OsDedaErkhR1MTIxJS9FjgIPCnhtcsM0o4IVEnQk0"
+    config.access_token = "2199777559-0cnOjV5oBtGl57FIWrtpNI5THdrRjyS9f9O9pQV"
+    config.access_token_secret = "hSs76D1kxjepVUuXoy0WinWisUyYdXifEbqNkd7IKCJZ2"
   end
   
-  puts "Cleared for takeoff"
+  puts "Scanning..."
 
-  client.filter(:track => "guardiancrime") do |tweet|
+  client.filter(:track => "guardianbot") do |tweet|
     puts "@#{tweet.user.screen_name}: #{tweet.text}"
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key = "kao1cND8RFCHcUJeb5xONcsZQ"
-      config.consumer_secret = "7L6jS0vRnqVVT9WpH7BZ3jPvrYgmd3XinK3tScVEov48wDgzeg"
-      config.access_token = "2199777559-2Eetn8D3VvuLkVKPvWsUA1jNaIgMoDu9F0gBZCT"
-      config.access_token_secret = "7V14U9JLsKEHvdAMpwrqDNuQsVuOnFUGgrO106b6GIOMS"
+      config.consumer_key = "TWPsNBca4joN3o3bF5GP8X52J"
+      config.consumer_secret = "k1O9YO267OsDedaErkhR1MTIxJS9FjgIPCnhtcsM0o4IVEnQk0"
+      config.access_token = "2199777559-0cnOjV5oBtGl57FIWrtpNI5THdrRjyS9f9O9pQV"
+      config.access_token_secret = "hSs76D1kxjepVUuXoy0WinWisUyYdXifEbqNkd7IKCJZ2"
     end 
     client.update("@#{tweet.user.screen_name} hello!")
+    puts "Threat neutralized."
+    puts "Scanning..."
   end
 end
